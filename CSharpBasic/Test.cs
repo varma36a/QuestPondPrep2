fd4aa8706b3df7e77635f3f068e0b8740a2eff10
rohi <varma36a@gmail.com>
2023-12-11 18:17:53 +0530

    Delegates

---
+++ CSharpBasic/Test.cs
@@ -0,0 +1,44 @@
+
+
+MyFileSearch mfs = new MyFileSearch();
+//mfs.Search("E:\\Test");  //blocking call
+mfs.sendData = Receiver;
+Console.WriteLine("Search Started....");
+
+var res = Task.Run(() =>
+    {
+        mfs.Search("/Users/RohithGVMac/AZ-204");
+    }
+);
+Console.WriteLine("Continue Executing");
+Console.ReadLine();
+
+static void Receiver(string fileName)
+{
+    Console.WriteLine(fileName);
+}
+public class MyFileSearch
+{
+
+    public delegate void SearchMethod(string input);
+    public SearchMethod sendData = null;
+
+    public void Search(string dir)
+    {
+        try
+        {
+            foreach (var d in Directory.GetDirectories(dir))
+            {
+                foreach (var f in Directory.GetFiles(d))
+                {
+                    sendData(f);
+                }
+            }
+        }
+        catch (System.Exception)
+        {
+
+            throw;
+        }
+    }
+}
