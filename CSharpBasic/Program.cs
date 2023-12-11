92d0592ce81cbea831eb60932f7e9fe2a9e8c0e7
rohi <varma36a@gmail.com>
2023-12-11 17:00:22 +0530

    initial

---
+++ CSharpBasic/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Contracts;
+
+
+
+public interface ISwimmable
+{
+    public string Color { get; set; }
+
+    public void Swim();
+}
+
+public interface IFylable
+{
+    public string Color { get; set; }
+    public void Fly();
+
+}
+
+public abstract class Bird
+{
+    public void Speak()
+    {
+
+    }
+}
+
+
+public class Pegion : Bird, IFylable
+{
+
+    string color = "";
+    public string Color { get => this.Color; set => setColor(); }
+
+    public Pegion()
+    {
+        setColor();
+    }
+
+    public string setColor()
+    {
+        this.color = "red";
+        return this.color;
+    }
+    public void Fly()
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
